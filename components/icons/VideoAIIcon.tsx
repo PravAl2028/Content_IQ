fe8485b6a95eb6073bdbf4428efc428f9cb8fd46
HNik500 <nikhilhazari100@gmail.com>
2026-03-08 16:10:22 +0530

    small ui changes

--- components/icons/VideoAIIcon.tsx
+++ components/icons/VideoAIIcon.tsx
@@ -1,23 +1,25 @@
-import { VideoCameraIcon } from '@heroicons/react/24/outline'
+import React from "react"
+import { VideoCameraIcon } from "@heroicons/react/24/outline"
 
-export function VideoAIIcon(props: React.ComponentProps<'svg'>) {
+export function VideoAIIcon(props: React.ComponentProps<"svg">) {
+    const { className, width = 24, height = 24, strokeWidth, style, ...rest } = props
+
     return (
         <div
-            className={`relative flex items-center justify-center ${props.className || ''}`}
-            style={{ width: props.width || 24, height: props.height || 24 }}
+            className={`relative flex items-center justify-center ${className || ""}`}
+            style={{ width, height }}
         >
-            {/* Main Video Icon */}
             <VideoCameraIcon
-                strokeWidth={props.strokeWidth}
-                style={{ ...props.style, width: '100%', height: '100%' }}
+                strokeWidth={strokeWidth}
+                style={{ width: "100%", height: "100%", ...style }}
                 className="text-current"
+                {...rest}
             />
 
-            {/* Bigger AWS Smile */}
             <svg
                 viewBox="0 0 256 256"
-                className="absolute -top-3 right-0.9  w-14  h-5"
-                xmlns="https://www.svgrepo.com/svg/370653/social-amazon"
+                className="absolute -top-1.2 right-2 w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
             >
                 <path
                     d="M60 170c40 28 100 28 140-2"
